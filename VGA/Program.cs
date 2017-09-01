9c3c1b3b0b7ede64ce829015d9fbf6b26a1cea1c
Carl Johnsen <carljohannesjohnsen@yahoo.dk>
2017-09-01 13:34:36 +0200

    Added SME references

--- VGA/Program.cs
+++ VGA/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SME;
 
 namespace VGA
 {
@@ -10,6 +6,10 @@ namespace VGA
     {
         static void Main(string[] args)
         {
+            new Simulation()
+                /*.BuildCSVFile()
+                .BuildVHDL()*/
+                .Run(typeof(Program).Assembly);
         }
     }
 }
